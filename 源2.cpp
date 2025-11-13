c549087df41399d98400608f827ccbe5e6cf8e22
jz <2115611757@qq.com>
2025-11-13 19:52:17 +0800

    Update 源2.cpp

--- 源2.cpp
+++ 源2.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using   使用    使用命名空间std；namespace   名称空间 std;
 int    Int main   主要() {main   主要() {
-	const double PI = 3.14;
-	double r;
-	double h;
+	const double PI = 3.14;   const   常量 double   双 PI = 3.14；
+	double r;   双r;
+	double h;   双h;
 	cout << "请输入圆锥的半径:" << endl;
-	cin >> r;
+	cin >> r;   Cin >> r；
 	cout << "请输入圆锥的高:" << endl;
-	cin >> h;
+	cin >> h;   Cin >> h；
 	cout << "圆锥的体积是: " << (PI * r * r * h) / 3;
 	return 0;
 }
+
