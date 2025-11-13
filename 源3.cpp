20da821a09542b6b908c4bf39f361ba2d610a9be
jz <2115611757@qq.com>
2025-11-13 19:53:06 +0800

    Update 源3.cpp

--- 源3.cpp
+++ 源3.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-using namespace std;
-int main() {
-	cout << "char length:" << sizeof(char) << endl;
-	cout << "int length:" << sizeof(int) << endl;
-	cout << "float length:" << sizeof(float) << endl;
-	cout << "bool length:" << sizeof(bool) << endl;
+using   使用    使用命名空间std；namespace   名称空间 std;
+int    Int main   主要() {main   主要() {
+	cout << Cout << "char length:"   “字符长度: << sizeof(char) << endl；"char length:"   “字符长度: << sizeof(char) << endl;   运算符
+	cout << Cout << int length:" << sizeof(int) << endl；"int length:"   “int长度: << sizeof(int) << endl;   运算符
+	cout << Cout << "float length:"   “浮动长度:“ << sizeof(float) << endl；"float length:"   “浮动长度:“ << sizeof(float) << endl;   运算符
+	cout << Cout << "bool length:"   “bool长度: << sizeof(bool) << endl；"bool length:"   “bool长度: << sizeof(bool) << endl;   运算符
+	return 0;
 }
