5392798e29d13dc69482f1f868e6118114844e89
jz <2115611757@qq.com>
2025-11-13 19:26:47 +0800

    Add files via upload

---
+++ 源3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "char length:" << sizeof(char) << endl;
+	cout << "int length:" << sizeof(int) << endl;
+	cout << "float length:" << sizeof(float) << endl;
+	cout << "bool length:" << sizeof(bool) << endl;
+}
