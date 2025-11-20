f25a937a26ef7f05b0da0af45f35d1eb4cb0d292
jz <2115611757@qq.com>
2025-11-20 16:18:37 +0800

    Delete 源3.cpp

--- 源3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using   使用    使用命名空间std；namespace   名称空间 std;
-int    Int main   主要() {main   主要() {
-	cout << Cout << "char length:"   “字符长度: << sizeof(char) << endl；"char length:"   “字符长度: << sizeof(char) << endl;   运算符
-	cout << Cout << int length:" << sizeof(int) << endl；"int length:"   “int长度: << sizeof(int) << endl;   运算符
-	cout << Cout << "float length:"   “浮动长度:“ << sizeof(float) << endl；"float length:"   “浮动长度:“ << sizeof(float) << endl;   运算符
-	cout << Cout << "bool length:"   “bool长度: << sizeof(bool) << endl；"bool length:"   “bool长度: << sizeof(bool) << endl;   运算符
-	return 0;
-}
